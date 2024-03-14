ce0914c15941ae0db363eb155d9642ac12ca8f4d
Satoshi_Nishimura <s104nishimura@gmail.com>
2024-03-14 18:32:21 +0900

    StateTreeの検証開始

---
+++ CW/Source/CW/Scene/StateTree/CWMatchingTask.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/StateTreeTaskBlueprintBase.h"
+#include "CWMatchingTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CW_API UCWMatchingTask : public UStateTreeTaskBlueprintBase
+{
+	GENERATED_BODY()
+	
+};
